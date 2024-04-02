0d57d1d151d805f87208ab0ba9a283039d53c6f5
Benjamin Lee <benjaminlee@Benjamins-MacBook-Pro.local>
2024-04-02 17:58:59 -0500

    inital commit, going fast

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react"
 
 function App() {
+  const [gameState, setGameState] = useState("pickAColor") 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div class="cootie-catcher closed vertical">
+        <div class="inner-flaps">
+          <div class="inner-flap top-left"></div>
+          <div class="inner-flap top-right"></div>
+          <div class="inner-flap bottom-left"></div>
+          <div class="inner-flap bottom-right"></div>
+        </div>
+        <div class="outer-flaps">
+          <div
+            class="outer-flap top-left clickable"
+          />
+          <div
+            class="outer-flap top-right clickable"
+          />
+          <div
+            class="outer-flap bottom-left clickable"
+          />
+          <div
+            class="outer-flap bottom-right clickable"
+          />
+        </div>
+      </div>
+      <button 
+        disabled={gameState === "pickAColor"}
+        id="move-button">
+          Pick a Color
+      </button>
+
+      
     </div>
   );
 }
